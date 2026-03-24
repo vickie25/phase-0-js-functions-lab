783af8c414b5c064fe21bf584fa0f4ce96ee7013
vickie25 <nyandorovictor3900@gmail.com>
2026-03-24 15:30:15 +0300

    Implement JavaScript functions for budgeting lab

--- index.js
+++ index.js
@@ -2,6 +2,25 @@
 
 
 
+function calculateTax(amount) {
+  return amount * 0.1;
+}
+
+function convertToUpperCase(text) {
+  return text.toUpperCase();
+}
+
+function findMaximum(num1, num2) {
+  return num1 > num2 ? num1 : num2;
+}
+
+function isPalindrome(word) {
+  return word === word.split('').reverse().join('');
+}
+
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  return originalPrice - (originalPrice * discountPercentage / 100);
+}
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
